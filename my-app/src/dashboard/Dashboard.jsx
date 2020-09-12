087ff009105247dbe2daec221ead155e2bea9d38
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2020-09-12 02:17:28 -0700

    lint

--- my-app/src/dashboard/Dashboard.jsx
+++ my-app/src/dashboard/Dashboard.jsx
@@ -1,39 +1,65 @@
 import React, { Component } from "react";
 import { Chart } from "react-google-charts";
 
+const SAMPLE_RESPONSE = {
+  all_ride_totals: {
+    count: 77,
+    distance: 832530,
+    elapsed_time: 193182,
+    elevation_gain: 2444,
+    moving_time: 166462,
+  },
+  all_run_totals: {
+    count: 204,
+    distance: 1111742,
+    elapsed_time: 404317,
+    elevation_gain: 4547,
+    moving_time: 392026,
+  },
+};
+
+function buildDataArray(response) {
+  const keys = [
+    "count",
+    "distance",
+    "elapsed_time",
+    "elevation_gain",
+    "moving_time",
+  ];
+  const { all_run_totals, all_ride_totals } = response;
+  const dataRows = keys.map((key) => [
+    key,
+    all_ride_totals[key],
+    all_run_totals[key],
+  ]);
+  return [["", "Ride Totals", "Run Totals"]].concat(dataRows);
+}
+
 class Dashboard extends Component {
   render() {
     return (
       <div className="display">
         <div style={{ display: "flex", maxWidth: 900 }}>
           <Chart
-            width={400}
+            width={600}
             height={300}
             chartType="ColumnChart"
             loader={<div>Loading Chart</div>}
-            data={[
-              ["City", "2010 Population", "2000 Population"],
-              ["New York City, NY", 8175000, 8008000],
-              ["Los Angeles, CA", 3792000, 3694000],
-              ["Chicago, IL", 2695000, 2896000],
-              ["Houston, TX", 2099000, 1953000],
-              ["Philadelphia, PA", 1526000, 1517000],
-            ]}
+            data={buildDataArray(SAMPLE_RESPONSE)}
             options={{
-              title: "Population of Largest U.S. Cities",
-              chartArea: { width: "30%" },
+              title: "Lifetime Strava Activity Breakdown",
+              chartArea: { width: "50%" },
               hAxis: {
-                title: "Total Population",
+                title: "Statistics",
                 minValue: 0,
               },
               vAxis: {
-                title: "City",
+                title: "",
               },
             }}
             legendToggle
           />
         </div>
-        <div className="display__text"> {this.props.text} </div>
       </div>
     );
   }
